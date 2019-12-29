f8da11ec089454be29d86a022e676599a90aa4f1
Marcio <marcioomonte@gmail.com>
2019-12-29 00:31:44 -0300

    creating user model, models loader and db connection

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Marcio' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Marcio',
+    email: 'marcioomonte@gmail.com',
+    password_hash: '1234234',
+  });
+  return res.json(user);
 });
 
 export default routes;
