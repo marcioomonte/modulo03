f3a91dbc1a1df22a31c290e639d883715b5e081e
Marcio <marcioomonte@gmail.com>
2020-01-02 03:09:44 -0300

    config mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-
+import mongoose from 'mongoose';
 import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
@@ -10,12 +10,22 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
   init() {
     this.connection = new Sequelize(databaseConfig);
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
