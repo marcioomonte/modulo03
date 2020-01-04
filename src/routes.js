bfaa423b8cf80587087d4eb82f8091a9f978cfba
Marcio <marcioomonte@gmail.com>
2020-01-04 13:26:23 -0300

    appointment delete (cancel)

--- src/routes.js
+++ src/routes.js
@@ -26,10 +26,13 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
 
-routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
 routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.get('/schedules', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
+
 export default routes;
