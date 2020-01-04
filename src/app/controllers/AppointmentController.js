5f231bae861d913e105cfc9e4d86f75bdc476e66
Marcio <marcioomonte@gmail.com>
2020-01-04 13:46:27 -0300

    config nodemailer

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,6 +2,8 @@ import Appointment from '../models/Appointment';
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
+import Mail from '../../lib/mail';
+
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
@@ -97,7 +99,9 @@ class AppointmentController {
     return res.json(appointment);
   }
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [{ model: User, as: 'provider', attributes: ['name', 'email'] }],
+    });
     if (appointment.user_id !== req.userId) {
       return res.json({
         error: "You don't have permission do cancel this appointment",
@@ -115,6 +119,12 @@ class AppointmentController {
     appointment.canceled_at = new Date();
 
     await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado.',
+      text: 'Você tem um novo cancelamento',
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '282b462cc8f2d9',
+    pass: '02daa56f849249',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
